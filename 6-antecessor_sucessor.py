3208a8375e970bab1a1fc769842ec5c71ec80112
Fp19se <filipe.s.almeida6@ba.estudante.senai.br>
2026-08-21 16:45:55 -0300

    Add files via upload

---
+++ 6-antecessor_sucessor.py
@@ -0,0 +1,17 @@
+import os
+
+os.system('cls')
+
+print('SOLICITANDO NUMERO')
+numero = int(input('Digite um numero: '))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+
+
+
+
+print('EXIBINDO NUMERO')
+print('numero antecessor: ', antecessor)
+print('numero sucessor: ', sucessor)
